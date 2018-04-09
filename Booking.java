c7109403827c7930bc4364db055491b0a27ed11c
Tomk1111 <tomk.tk53@gmail.com>
2018-04-09 12:12:27 +0100

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,96 +1,53 @@
 import java.util.*;
-import java.text.*;
-import javax.swing.*;
 public class Booking
 {
 	public int bookingId;
 	public int facilityId;
 	public int userId;
-	public Date date;
-	public int slotNumber; // e.g 9am - 10am is slot 1.
-	public char paymentStatus;
-	
-	public Booking(int bookingId,int facilityId,int userId,Date date,int slotNumber,char paymentStatus)
+	public Date bookingDate;
+	public int bookingSlot;
+	public boolean paymentStatus;
+	//Bookingdate maybe as Date*****FIX LIKE USER/FACILITY
+	public Booking(int bookingId, int facilityId, int userId, Date bookingDate, int bookingSlot, boolean paymentStatus)
 	{
-	bookingId++;
-	this.facilityId=facilityId;
-	this.userId=userId;
-	this.date=date;
-	this.slotNumber=slotNumber;
-	this.paymentStatus=paymentStatus;
+		this.bookingId 		= bookingId;
+		this.facilityId 	= facilityId;
+		this.userId 		= userId;
+		this.bookingDate	= bookingDate;
+		this.bookingSlot 	= bookingSlot;
+		this.paymentStatus	= paymentStatus;
 	}
 	
 	public int getBookingId()
 	{
-		return this.bookingId;
+		return bookingId;
 	}
 	
 	public int getFacilityId()
 	{
-		return this.facilityId;
+		return facilityId;
 	}
 	
 	public int getUserId()
 	{
-		return this.userId;
+		return userId;
 	}
 	
-	public Date getDate()
+	public Date getBookingDate()
 	{
-		return this.date;
+		return bookingDate;
 	}
 	
-	public int getSlotNumber()
+	public int getBookingSlot()
 	{
-		return this.slotNumber;
+		return bookingSlot;
 	}
 	
-	public char getPaymentStatus()
+	public boolean getPaymentStatus()
 	{
-		return this.paymentStatus;
+		return paymentStatus;
 	}
 	
-	public void setBookingId(int bookingId)
-	{
-		this.bookingId=bookingId;
-	}
 	
-	public void setFacilityId(int facilityId)
-	{
-		this.facilityId=facilityId;
-	}
-	
-	public void setUserId(int userId)
-	{
-		this.userId=userId;
-	}
-	
-	public void setDate(String date)
-	{
-		try
-		{
-			SimpleDateFormat aFormatter=new SimpleDateFormat("dd/MM/yyyy");
-			this.date=aFormatter.parse(date);
-		}
-		catch(ParseException e)
-		{
-			JOptionPane.showMessageDialog(null,"Error when parsing data");
-		}
-	}
-	
-	public void setSlotNumber(int slotNumber)
-	{
-		this.slotNumber=slotNumber;
-	}
-	
-	public void setPaymentStatus(char paymentStatus)
-	{
-		this.paymentStatus=paymentStatus;
-	}
-	
-	public String bookingToString()
-	{
-		String info=""+bookingId+","+facilityId+","+userId+","+date+","+slotNumber+","+paymentStatus;
-		return info;
-	}
+	//SETS TO BE ADDED
 }
