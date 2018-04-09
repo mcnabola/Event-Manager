b69ae1e5c885199482bc2f785a06e280892d3f21
Tomk1111 <tomk.tk53@gmail.com>
2018-04-09 12:12:59 +0100

    Update User.java

--- User.java
+++ User.java
@@ -3,59 +3,62 @@ public class User
 	public int userId;
 	public String email;
 	public String password;
-	public int userType;
-	
-	public User(int userId,String email,String password,int userType)
+	public int userType; //1 = admin, 2 = user
+
+	public User(int userId, String email, String password, int userType) //For restoring users
 	{
-		this.userId = userId;
-		this.email=email;
-		this.password=password;
-		this.userType=userType;
+		this.userId 	= userId;
+		this.email		= email;
+		this.password 	= password;
+		this.userType	= userType;
+	}
+	
+	public User(int userId, String email) //Basic for adding new user
+	{
+		this.userId 	= userId;
+		this.email 		= email;
+		this.password	= generatePassword();
+		this.userType 	= 2;
+	}
+	
+	
+	public static String generatePassword() //Basic alpha-numeric password generator
+	{
+		String password = "";
+		String alpha    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		int passLength  = 8;
+		int index 		= 0;
+		for (int i=0;i<passLength;i++)
+		{
+			index 		= (int)(Math.random()* alpha.length());
+			password	+= alpha.substring(index, index+1);
+		}
+		return password;
 	}
 
 	public int getUserId()
 	{
-		return this.userId;
+		return userId;
 	}
 	
 	public String getEmail()
 	{
-		return this.email;
+		return email;
 	}
-	
+
 	public String getPassword()
 	{
-		return this.password;
+		return password;
 	}
 	
 	public int getUserType()
 	{
-		return this.userType;
+		return userType;
 	}
 	
-	public void setUserId(int userId)
+public String userToString()
 	{
-		this.userId=userId;
-	}
-	
-	public void setEmail(String email)
-	{
-		this.email=email;
-	}
-	
-	public void setPassword(String password)
-	{
-		this.password=password;
-	}
-	
-	public void setUserType(int userType)
-	{
-		this.userType=userType;
-	}
-	
-	public String userToString()
-	{
-		String info=""+userId+","+email+","+password+","+userType;
+		String info	= userId + "," + email + "," + password + "," + userType;
 		return info;
 	}
 }
