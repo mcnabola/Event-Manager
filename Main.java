ad21e65eb24acd137390408ab0cfb9e7f6310325
Tomk1111 <tomk.tk53@gmail.com>
2018-04-15 15:52:16 +0100

    Update Main.java
    Put in my arraylist login method again - When updating, only copy in the new methods

--- Main.java
+++ Main.java
@@ -235,52 +235,43 @@ public class main
 		return isValid;
 	}
 	
-	public static boolean loginMethod(String username, String password)
+		public static boolean loginMethod(String email, String password) 
 	{
-		File userFile = new File(userFileName);
-		Scanner in;
-		String[] fileElements;
 		boolean found = false;
 		int loginAttempts = 3;
 		try
 		{
-			if (userFile.exists())
+			while(!found)
 			{
-				while (!found)
+				for (int i=0;i<users.size();i++)
 				{
-					in = new Scanner(userFile);
-					while (in.hasNext())
+					if (users.get(0).getEmail().equalsIgnoreCase(email) && users.get(i).getPassword().equals(password))
 					{
-						fileElements = in.nextLine().split(",");
-						if (username.equals(fileElements[1]) && password.equals(fileElements[2]))
-						{
-							found = true;
-							currentID = Integer.parseInt(fileElements[0]);
-							userType = Integer.parseInt(fileElements[3]);
-						}
-					}
-					in.close();
-					if (!found)
-					{
-						loginAttempts--; 
-						if (loginAttempts == 0)
-						{
-							JOptionPane.showMessageDialog(null, "No attempts remaining.");
-							break;
-						}
-						 JOptionPane.showMessageDialog(null, "Incorrect login details.\n" + loginAttempts + " attempt(s) remaining.");
-						username = JOptionPane.showInputDialog(null, "Enter username");
-						password = JOptionPane.showInputDialog(null,"Enter password");
-					}
-					else
-						JOptionPane.showMessageDialog(null, "Successfully logged in as " + username);	
+						found = true;
+						currentUserId 	= users.get(i).getUserId();
+						currentUserType = users.get(i).getUserType();
+					}	
 				}
+				if (!found)
+				{
+					loginAttempts--;
+					if (loginAttempts == 0)
+					{
+						JOptionPane.showMessageDialog(null, "No attempts remaining");
+						break;
+					}
+					JOptionPane.showMessageDialog(null, "Incorrect login details.\n" + loginAttempts + " attempts remaining:");
+					email	 = JOptionPane.showInputDialog(null, "Enter email");
+					password = JOptionPane.showInputDialog(null, "Enter password");
+				}
+				else
+					JOptionPane.showMessageDialog(null, "Successfully logged in as " + email);
 			}
 		}
 		catch(Exception e)
 		{}
 		return found;
-	} 
+	}
 	
 		public static void createNewFacility()
 	{
