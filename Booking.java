e1f5ead92bea5359c3e101549d9a1f1fd0764e69
seanmorrissey98 <35046602+seanmorrissey98@users.noreply.github.com>
2018-04-21 19:02:42 +0100

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -2,6 +2,12 @@ import java.util.*;
 import java.text.*;
 import java.time.*;
 import java.time.format.*;
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/	
 public class Booking
 {
 	public int bookingId;
@@ -13,7 +19,13 @@ public class Booking
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	//FOR RESTORING - MAY ADD FURTHER CONSTRUCTORS
-	public Booking(int bookingId, int facilityId, int userId, LocalDate bookingDate, int bookingSlot, boolean paymentStatus)
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/	
+	   public Booking(int bookingId, int facilityId, int userId, LocalDate bookingDate, int bookingSlot, boolean paymentStatus)
 	{
 		this.bookingId 		= bookingId;
 		this.facilityId 	= facilityId;
@@ -22,59 +34,94 @@ public class Booking
 		this.bookingSlot 	= bookingSlot;
 		this.paymentStatus	= paymentStatus;
 	}
-	
-	public Booking(int bookingId, int facilityId, int userId, String bookingDate, int bookingSlot, boolean paymentStatus)
-	{
-		this.bookingId 		= bookingId;
-		this.facilityId 	= facilityId;
-		this.userId 		= userId;
-		this.bookingDate	= LocalDate.parse(bookingDate, formatter);
-		this.bookingSlot 	= bookingSlot;
-		this.paymentStatus	= paymentStatus;
-	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public int getBookingId()
 	{
 		return bookingId;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public int getFacilityId()
 	{
 		return facilityId;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public int getUserId()
 	{
 		return userId;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public LocalDate getBookingDate()
 	{
 		return bookingDate;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public int getBookingSlot()
 	{
 		return bookingSlot;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public boolean getPaymentStatus()
 	{
 		return paymentStatus;
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public String bookingToString()
 	{
 		String temp = bookingDate.format(formatter);
 		String info = bookingId + "," + facilityId + "," + userId + "," + temp + "," + bookingSlot + "," + paymentStatus;
 		return info;
 	}
-
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/	
 	public void setBookingDate(String aDate)
 	{
 		this.bookingDate = LocalDate.parse(aDate);
 	}
-	
+	 /**
+	   *
+	   *Input -
+	   *Output -
+	   *
+	   **/		
 	public void setBookingDate(LocalDate aDate)
 	{
 		this.bookingDate = aDate;
