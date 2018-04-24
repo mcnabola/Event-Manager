13b8b17e04893be18a2762159de151fb62fd6de5
paulmurphy291 <36921919+paulmurphy291@users.noreply.github.com>
2018-04-24 21:50:18 +0100

    Update User.java

--- User.java
+++ User.java
@@ -11,9 +11,10 @@ public class User
 	public String password;
 	public int userType; //1 = admin, 2 = user
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Constructor method for user.
+	   *Gets the parameters and assign them to the variables of user.
+	   *Input -Int of user Id,String of user's email ,String of user's password, int of usertype
+	   *Output -No output
 	   *
 	   **/	
 	public User(int userId, String email, String password, int userType) //For restoring users
@@ -24,9 +25,11 @@ public class User
 		this.userType	= userType;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Gets the parameters and assign them to the variables of user.
+	   *Calls the generatepassword method to create password.
+	   *Automatically assigns usertype to 2.
+	   *Input -Int user Id and String email
+	   *Output -No output
 	   *
 	   **/		
 	public User(int userId, String email) //Basic for adding new user
@@ -38,9 +41,9 @@ public class User
 	}
 	
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Method to generate a random String password for user.
+	   *Input -None
+	   *Output -returns the genrated password as a string
 	   *
 	   **/		
 	public static String generatePassword() //Basic alpha-numeric password generator
@@ -57,9 +60,9 @@ public class User
 		return password;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Get method to return userid of user
+	   *Input -No input
+	   *Output -returns int UserId of user
 	   *
 	   **/	
 	public int getUserId()
@@ -67,9 +70,9 @@ public class User
 		return userId;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Get method to return Email of user
+	   *Input -No input
+	   *Output -returns String Email of user
 	   *
 	   **/		
 	public String getEmail()
@@ -77,9 +80,9 @@ public class User
 		return email;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Get method to return Password of user
+	   *Input -No input
+	   *Output -returns String Password of user
 	   *
 	   **/	
 	public String getPassword()
@@ -87,9 +90,9 @@ public class User
 		return password;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Get method to return Usertype of user
+	   *Input -No input
+	   *Output -returns int Uertype of user
 	   *
 	   **/		
 	public int getUserType()
@@ -97,9 +100,9 @@ public class User
 		return userType;
 	}
 	 /**
-	   *
-	   *Input -
-	   *Output -
+	   *Get method to return User info to String
+	   *Input -No input
+	   *Output -returns a String of the user's info
 	   *
 	   **/		
 	public String userToString()
