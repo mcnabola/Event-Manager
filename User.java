00e63d45e380cac25beef44acdafa0df11e55894
mcnabola <35931286+mcnabola@users.noreply.github.com>
2018-04-25 23:02:12 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,46 +1,39 @@
-	 /**
-	   *Public class User, Variables for creating object of User Type
-	   *
-	   *
-	   *
-	   **/	
+/**
+ * Class created for describing a User Object 
+ */	
 public class User
 {
 	public int userId;
 	public String email;
 	public String password;
 	public int userType; //1 = admin, 2 = user
-	 /**
-	   *Constructor method for user.
-	   *Gets the parameters and assign them to the variables of user.
-	   
-	   @param-Int userId is the Id of user
-	   @param-String email is the email of user
-	   @param-String password is the password of user
-	   @param-int usertype is the type of user-1=admin,2=user
-	   
-	   *@return-None
-	   *
-	   **/	
-	public User(int userId, String email, String password, int userType) //For restoring users
+	 
+	/**
+	 *Constructor method for restoring users from the text file and to be loaded into the arrayList.
+	 *Gets the parameters and assign them to the variables of the user object. 
+	 *
+	 *@param userId is the Id of user
+	 *@param email is the email of user
+	 *@param password is the password of user
+	 *@param usertype is the type of user-1=admin,2=user
+	 */	
+	public User(int userId, String email, String password, int userType) 
 	{
 		this.userId 	= userId;
 		this.email		= email;
 		this.password 	= password;
 		this.userType	= userType;
 	}
-	 /**
-	   *Gets the parameters and assign them to the variables of user.
-	   *Calls the generatepassword method to create password.
-	   *Automatically assigns usertype to 2.
-	   
-	   @param-Int userId is the Id of user
-	   @param-String email is the email of user
-	   
-	   @return-None
-	   *
-	   **/		
-	public User(int userId, String email) //Basic for adding new user
+	/**
+	 * This is the basic method used for craeting a user while the program is operation.
+	 *Calls the generatepassword method to create password.
+	 *Automatically assigns usertype to 2.
+	 *
+	 *@param-Int userId is the Id of user
+	 *@param-String email is the email of user
+	 *
+	 */		
+	public User(int userId, String email) //For adding new user
 	{
 		this.userId 	= userId;
 		this.email 		= email;
@@ -48,15 +41,13 @@ public class User
 		this.userType 	= 2;
 	}
 	
-	 /**
-	   *Method to generate a random String password for user.
-	   
-	   *@param -None
-	   
-	   *@return -returns the genrated password as a string
-	   *
-	   **/		
-	public static String generatePassword() //Basic alpha-numeric password generator
+	/**
+	 *Method to generate a random alpha-numeric password generator used in the creation of a user. 
+	 *
+	 *@return    Returns the generated password as a string.
+	 *
+	 */		
+	public static String generatePassword() 
 	{
 		String password = "";
 		String alpha    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -69,62 +60,53 @@ public class User
 		}
 		return password;
 	}
-	 /**
-	   *Get method to return userid of user
-	   
-	   *@param -None
-	   
-	   *@return -returns int UserId of user
-	   *
-	   **/	
+	/**
+	  *Get method to return userid of user. 
+	  * 
+	  *@return userId returns the users id number.
+	  *
+	  */	
 	public int getUserId()
 	{
 		return userId;
 	}
-	 /**
-	   *Get method to return Email of user
-	   
-	   *@param -None
-	   
-	   *@return -returns String Email of user
-	   *
-	   **/		
+	/**
+	  *Get method to return Email of user
+	  *  
+	  *@return email returns a String of the users email address.
+	  *
+	  */		
 	public String getEmail()
 	{
 		return email;
 	}
-	 /**
-	   *Get method to return Password of user
-	   
-	   *@param -None
-	   
-	   *@return -returns String Password of user
-	   *
-	   **/	
+	/**
+	  *Get method to return Password of user. 
+	  *
+	  *
+	  *@return password String Password of user
+	  *
+	  */	
 	public String getPassword()
 	{
 		return password;
 	}
-	 /**
-	   *Get method to return Usertype of user
-	   
-	   *@param -None
-	   
-	   *@return-returns int Uertype of user
-	   *
-	   **/		
+	/**
+	 *Get method to return the Usertype of user, Type 1 or 2 depending on whether the user is a Admin or not
+	 *
+	 *@return userType returns a int value of the users admin status type.
+	 *
+	 */		
 	public int getUserType()
 	{
 		return userType;
 	}
-	 /**
-	   *Get method to return User info to String
-	   
-	   *@param -None
-	   
-	   *@return-returns a String of the user's info
-	   *
-	   **/		
+	/**
+	 *Get method to return User info to String
+	 * 
+	 *@return info String containing all of the users info in one string seperated by commas.
+	 *
+	 */		
 	public String userToString()
 	{
 		String info	= userId + "," + email + "," + password + "," + userType;
