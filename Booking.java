6509e33f224d1244143413b304ef19103f39ea08
mcnabola <35931286+mcnabola@users.noreply.github.com>
2018-04-25 23:52:39 +0100

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.text.*;
 import java.time.*;
 import java.time.format.*;
-	 /**
-	   public Class Booking,variables for making objects of booking type
-	   *
-	   **/	
-public class Booking
-{
+	/**
+	 *Public Class for Bookings
+	 *
+	 */	
+	public class Booking
+	{
 	public int bookingId;
 	public int facilityId;
 	public int userId;
@@ -16,19 +16,17 @@ public class Booking
 	public boolean paymentStatus;
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	 /**
-	   *Construcutor method to create Booking object
-	   
-	   @param-Int bookingId is the Id of the booking
-	   @param-Int facility is the Id of the facility for which the booking is made for
-	   @param-Int UserId is the Id of the user who made the booking
-	   @param-LocalDate bookingDate is the date of when the booking is made for
-	   @param-Int bookingSlot is the position of the day/time the booking is made for
-	   @param-Boolean paymentStatus is the boolean of whether the user has paid for this booking or not
-	   
-	   *@return-None
-	   *
-	   **/	
+	/**
+	 *Construcutor method to create Booking object
+	 * 
+	 *@param bookingId Input is the Id of the booking
+	 *@param facilityId Input is the Id of the facility for which the booking is made for
+	 *@param userId Input is the Id of the user who made the booking
+	 *@param bookingDate Inputs is the date of when the booking is made for in the LocalDate type.
+	 *@param bookingSlot Input is the position of the day/time the booking is made for
+	 *@param paymentStatus Inputs the boolean of whether the user has paid for this booking or not
+	 *
+	 */	
 	   public Booking(int bookingId, int facilityId, int userId, LocalDate bookingDate, int bookingSlot, boolean paymentStatus)
 	{
 		this.bookingId 		= bookingId;
@@ -39,18 +37,16 @@ public class Booking
 		this.paymentStatus	= paymentStatus;
 	}
 	/**
-	   *Construcutor method to create Booking object
-	   
-	   @param-Int bookingId is the Id of the booking
-	   @param-Int facility is the Id of the facility for which the booking is made for
-	   @param-Int UserId is the Id of the user who made the booking
-	   @param-String bookingDate is the date of when the booking is made for
-	   @param-Int bookingSlot is the position of the day/time the booking is made for
-	   @param-Boolean paymentStatus is the boolean of whether the user has paid for this booking or not
-	   
-	   *@return-None
-	   *
-	   **/	
+	 *Construcutor method to create Booking object
+	 *  
+	 *@param bookingId is the Id of the booking
+	 *@param facility is the Id of the facility for which the booking is made for
+	 *@param UserId is the Id of the user who made the booking
+	 *@param bookingDate is the date of when the booking is made for
+	 *@param bookingSlot is the position of the day/time the booking is made for
+	 *@param paymentStatus is the boolean of whether the user has paid for this booking or not
+	 *
+	 */	
 	   public Booking(int bookingId, int facilityId, int userId, String bookingDate, int bookingSlot, boolean paymentStatus)
 	{
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -61,119 +57,108 @@ public class Booking
 		this.bookingSlot 	= bookingSlot;
 		this.paymentStatus	= paymentStatus;
 	}
-	 /**
-	   *Get method for BookingId of the booking made
-	   
-	   *@param -None
-	   
-	   *@return -Returns int of the BookingId
-	   *
-	   **/		
+	/**
+	 *Get method for BookingId of the booking made
+	 *    
+	 *@return getBookingId Returns the booking id of a booking object.
+	 *
+	 */		
 	public int getBookingId()
 	{
 		return bookingId;
 	}
-	 /**
-	   *Get method for FacilityId of the booking
-	   
-	   *@param -None
-	   
-	   *@return -Returns int of the FacilityId
-	   *
-	   **/		
+	/**
+	 *Get method for FacilityId of the booking
+	 * 	   
+	 *@return facilityId Returns a int of the facilities id number.
+	 *
+	  */		
 	public int getFacilityId()
 	{
 		return facilityId;
 	}
-	 /**
-	   *Get method for UserId of the booking made
-	   
-	   *@param -None
-	   
-	   *@retutn -Returns int of the UserId that made the booking
-	   *
-	   **/		
+	/**
+	 *Get method for UserId of the booking made
+	 *  
+	 *@retutn userID Returns a int of the UserId that is made for the booking
+	 *
+	 */		
 	public int getUserId()
 	{
 		return userId;
 	}
-	 /**
-	   *Get method for the booking date of the booking made
-	   
-	   *@param -None
-	   
-	   *@return -Returns LocalDate of the day the booking is for
-	   *
-	   **/		
+	/**
+	 *Get method for the booking date of the booking made
+	 *  
+	 *@return bookingDate Returns a LocalDate of the day the booking is made for.
+	 *
+	 */		
 	public LocalDate getBookingDate()
 	{
 		return bookingDate;
 	}
-	 /**
-	   *Get method for the slot for which the booking is made
-	   
-	   *@param -None
-	   
-	   *@return -Returns int of the booking slot the booking is made for on that date
-	   *
-	   **/		
+	/**
+	 *Get method for the slot for which the booking is made
+	 *
+	 *  
+	 *@return bookingSlot The booking is made for on that date
+	 *
+	 */		
 	public int getBookingSlot()
 	{
 		return bookingSlot;
 	}
-	 /**
-	   *Get method for whether the user has paid for their booking or not
-	   
-	   *@param -None
-	   
-	   *return -Returns Boolean of PaymentStatus true if they have paid for booking
-	   *
-	   **/		
+	/**
+	 *Get method for whether the user has paid for their booking or not
+	 *    
+	 *return paymentStatus Boolean return based on payment status.
+	 *
+	 */		
 	public boolean getPaymentStatus()
 	{
 		return paymentStatus;
 	}
-	 /**
-	   *Mehtod to convert all Booking info made to one String
-	   *@param -None
-	   *@return -reurns String info,which contains all booking variables seperated by a ","
-	   *
-	   **/		
+	/**
+	 *Mehtod to convert all Booking info made to one String
+	 *
+	 *@return info Contains all booking variables seperated by a ","
+	 *
+	 */		
 	public String bookingToString()
 	{
 		String temp = bookingDate.format(formatter);
 		String info = bookingId + "," + facilityId + "," + userId + "," + temp + "," + bookingSlot + "," + paymentStatus;
 		return info;
 	}
-	 /**
-	   *Set Method to give a booking a date
-	   *
-	   *@param -String aDate is the date the user wishes to book for
-	   *@return -None
-	   *
-	   **/	
+	/**
+	 *Set Method to give a booking a date, alternative to the LocalDate
+	 *
+	 *@param aDate Date entered as a String is the date the user wishes to book for
+	 *
+	 *
+	 */	
 	public void setBookingDate(String aDate)
 	{
 		this.bookingDate = LocalDate.parse(aDate);
 	}
-	 /**
-	   *Set Method to give a booking a date
-	   *
-	   *@param LocalDate aDate is the date the user wishes to book for
-	   *@return -None
-	   *
-	   **/		
+	/**
+	 *Set Method to give a booking a date
+	 *
+	 *@param aDate date the user wishes to book for
+	 *
+	 *
+	 */		
 	public void setBookingDate(LocalDate aDate)
 	{
 		this.bookingDate = aDate;
 	}
 	/**
-	   *Set Method to give a booking a payment status
-	   *
-	   *@param Boolean paymentStatus of whether the user has paid for the booking or not
-	   *@return none
-	   *
-	   **/	
+	 *Set Method to give a booking a payment status
+	 *
+	 *@param paymentStatus Boolean of whether the user has paid for the booking or not
+	 *
+	 *
+	 */	
 	public void setPaymentStatus(boolean paymentStatus)
 	{
 	       this.paymentStatus=paymentStatus;
